3e3c9693fe3b135f04fa353dc9cc37a2a92716b4
Pragadeesh.A.S <aspragadeesh75@gmail.com>
2025-12-03 09:33:26 +0530

    leaders tomorrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
 import HelloWorldCom from "./hello-world/hello-world-com";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
 
 function App() {
   return (
-    // <HelloWorldCom/>
-    <FlexBoxComponent />
+    <div>
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      <LeadersTomorrowComponent />
+    </div>
   );
 }
 
