4a85e8f443cda4a1f9e3e803e7027b390362fbd6
Pragadeesh.A.S <aspragadeesh75@gmail.com>
2025-12-03 08:42:29 +0530

    created flex

--- src/hello-world/hello-world-style.js
+++ src/hello-world/hello-world-style.js
@@ -1,7 +1,23 @@
 import styled from "styled-components";
-export const HelloWorldStyle=styled.div`
-    background-color: red;
-    .Hello{
-        color: green;
-    }
+export const HelloWorldStyle = styled.div`
+  .parent {
+    display: flex;
+    flex-direction: column;
+    background-color: green;
+    //align-items: flex-start;
+    // align-items: center;
+    // align-items: flex-start;
+    //justify-content: flex-end;
+    align-items: flex-end;
+    //justify-content: space-evenly;
+    //justify-content: space-between;
+    //  justify-content: space-around;
+    gap: 20px;
+    height: 100vh;
+  }
+  .square__child {
+    width: 50px;
+    height: 50px;
+    background-color: yellow;
+  }
 `;
